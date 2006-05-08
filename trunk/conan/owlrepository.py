3b84fa2a327d46726b9b5e218a83d878dcd50cdb
carenini <carenini@1a00e267-dc06-0410-b807-9351c1609e06>
2006-05-08 16:28:13 +0000

    Conan: started building OWL Repository

--- trunk/conan/owlrepository.py
+++ trunk/conan/owlrepository.py
@@ -3,36 +3,70 @@
 
 from os import sys
 from repository import Repository
+import swap.cwm
 import RDF
 
 class OWLRepository(Repository):
+    """
+    OWL-enabled RDF repository
+    """
+    
+    # These two variables have to be defined in some config file!!!
+    owl_classes='http://veggente.berlios.de/think/owl.n3'
+    owl_rules='http://veggente.berlios.de/think/owl_rules.n3'
+    def add_document(self, uri, think=True, context=None, overwrite=True):
+        Repository.add_document(self,uri,context)
+        
+    def add_document(self, uri, think=True, context=None, overwrite=True):
+        Repository.add_document(self,uri,context)
+        if think==True:
+            self.exec_inference(uri,'think+'+uri)
+            
+    def exec_ontology_inference(self, uri_list,  results_uri):
         """
-        OWL-enabled RDF repository
+        @context_uri:char*
+        @results_uri:char* :Where to store inferred statements
         """
-        def __init__(self,db):
-                Repository.__init__(self)
-        
-        #public:
-        def add_document(self, uri, context=None):
-                Repository.add_document(self,uri,context)
-        
-        def exec_inference(self, context_uri,  results_uri):
-                """
-                @context_uri:char*
-                @results_uri:char* :Where to store inferred statements
-                """
-                return None
-        
-        def is_allowed(self, property_uri,  class_uri):
-                """
-                @property_uri:char*
-                @class_uri:char*
-                """
-                return None
+        target=self.find_deps()
+        cmd=[]
+        # I know it this UGLY, but if it works...
+        cmd.append('cwm')
+        cmd.append('--rdf')
+        for i in uri_list:
+            cmd.append(uri_list)
+        cmd.append(' --n3 ')
+        cmd.append(self.owl_classes)
+        cmd.append('--filter='+self.owl_rules)
+        cmd.append('--think')
+        cmd.append('--ugly')
+        cmd.append('--purge') 
+        cmd.append('--rdf')
+        cwm_out=cwm.doCommand(cmd)
+        if cwm_out!=None:
+            
+        return None
+    
+    def __find_location(self,uri):
+        """
+        Returns the original uri if the document is not in store, 
+        or the Redland-VFS uri if already stored
+        """
+        for i in self.model.get_contexts():
+            if i.uri==uri:
+                # URI is in store
+                return self.db_uri+uri
+        return uri
+    
+    def is_allowed(self, property_uri,  class_uri):
+        """
+        @property_uri:char*
+        @class_uri:char*
+        """
+        return None
 
 
 if __name__=="__main__":
-        print "Veggente project: Conan OWL repository"
-        repository=OWLRepository('.')
-        sys.exit(0)
+    print "Veggente project: Conan OWL repository"
+    repository=OWLRepository('.')
+    return 0
 
--- trunk/conan/repository.py
+++ trunk/conan/repository.py
@@ -5,104 +5,109 @@ import RDF
 
 # Standard RDF repository
 class Repository(object):
-        """
-        Standard RDF repository
-        """
-        #public:
-        db_dir='' # char*
-        parser=None
-        model=None
-        storage=None
-        
-        #constructor
-        def __init__(self,db='.'):
-                self.db_dir=db
-                self.parser=RDF.Parser('raptor')
-                if self.parser is None:
-                        raise "Failed initializing RDF parser"
-                if (self.__init_db()==0):
-                        print 'RDF repository initialized'
-                        
-        def __del__(self):
-                self.model.sync()
+    """
+    Standard RDF repository
+    """
+    #public:
+    db_dir='' # char*
+    db_name='conan'
+    parser=None
+    model=None
+    storage=None
+    db_uri=''
+    
+    #constructor
+    def __init__(self,dbname,db='.'):
+        self.db_dir=db
+        if (dbname!=None) and (dbname!=''):
+            self.db_name=dbname
+        self.parser=RDF.Parser('raptor')
+        if self.parser is None:
+                raise "Failed initializing RDF parser"
+        if (self.__init_db()==0):
+                print 'RDF repository initialized'
+        self.db_uri=self.db_name+'://'+self.db_dir+'+'
+                    
+    def __del__(self):
+        self.model.sync()
 
-        #public:
-        def add_document(self, uri,  context=None):
-                """
-                @uri:char*
-                @context:char*
-                """
-                current_uri=None
-                context_node=None
-                if uri is None:
-                        return -1
-                current_uri=RDF.Uri(uri)
-                if context is None:
-                        context_node=RDF.Node(RDF.Uri(string=uri))
-                else:
-                        context_node=RDF.Node(RDF.Uri(context))
-                for s in self.parser.parse_as_stream(current_uri,current_uri):
-                        self.model.add_statement(s,context_node)
-                self.model.sync()
-                print "Adding URI "+uri
-                
-                return 0
+    #public:
+    def add_document(self, uri,  context=None):
+        """
+        @uri:char*
+        @context:char*
+        """
+        current_uri=None
+        context_node=None
+        if uri is None:
+                return -1
+        current_uri=RDF.Uri(uri)
+        if context is None:
+                context_node=RDF.Node(RDF.Uri(string=uri))
+        else:
+                context_node=RDF.Node(RDF.Uri(context))
+        for s in self.parser.parse_as_stream(current_uri,current_uri):
+                self.model.add_statement(s,context_node)
+        self.model.sync()
+        print "Adding URI "+uri
         
-        def remove_document(self, context): 
-                """
-                @context:char*
-                """
-                if (context!=''):
-                        if (self.model.remove_statements_with_context(RDF.Uri(context))!=0):
-                                print "Error removing URI "+context
-                return None
-        
-        def query_model(self, query_str):
-                """
-                @query_str:char*
-                @context:char*
-                """
-                if query_str!=None:
-                    q=RDF.SPARQLQuery(query_str)
-                    return q.execute(self.model)
-                return None
-        
-        def check_documents(self):
-                """
-                Lists documents in store
-                """
-                print "Stored URI list:"
-                for i in self.model.get_contexts():
-                        print i
-                return None
+        return 0
+    
+    def remove_document(self, context): 
+        """
+        @context:char*
+        """
+        if (context!=''):
+                if (self.model.remove_statements_with_context(RDF.Uri(context))!=0):
+                        print "Error removing URI "+context
+        return None
+    
+    def query_model(self, query_str):
+        """
+        @query_str:char*
+        @context:char*
+        """
+        if query_str!=None:
+            q=RDF.SPARQLQuery(query_str)
+            return q.execute(self.model)
+        return None
+    
+    def check_documents(self):
+        """
+        Lists documents in store
+        """
+        print "Stored URI list:"
+        for i in self.model.get_contexts():
+            print i.uri
+        return None
 
-        def check_class(self, uri):
-                """
-                @uri:char*
-                """
-                return None
-        
-        def check_property(self, uri): 
-                """
-                @uri:char*
-                """
-                return None
-        
-        def __init_db(self):
-                storage_options="hash-type='bdb',contexts='yes',dir='"+self.db_dir+"'"
-                self.storage=RDF.Storage(storage_name="hashes",
-                                name="conan",
-                                options_string=storage_options)
-                if self.storage is None:
-                        raise "Failed creating storage"
-                self.model=RDF.Model(self.storage)
-                if self.model is None:
-                        raise "Failed creating RDF model"
-                return 0
+    def check_class(self, uri):
+        """
+        @uri:char*
+        """
+        return None
+    
+    def check_property(self, uri): 
+        """
+        @uri:char*
+        """
+        return None
+    
+    def __init_db(self):
+        storage_options="hash-type='bdb',contexts='yes',dir='"+self.db_dir+"'"
+        self.storage=RDF.Storage(storage_name="hashes",
+                        name=self.db_name,
+                        options_string=storage_options)
+        if self.storage is None:
+                raise "Failed creating storage"
+        self.model=RDF.Model(self.storage)
+        if self.model is None:
+                raise "Failed creating RDF model"
+        return 0
 
 if __name__=="__main__":
         print "Veggente project: Conan RDF repository"
-        repository=Repository('.')
-        repository.add_document('http://veggente.berlios.de/ns/RIMOntology')
-        repository.add_document('file:///tmp/foaf.rdf')
+        repository=Repository('conan','.')
+        #repository.add_document('http://veggente.berlios.de/ns/RIMOntology')
+#        repository.add_document('file:///tmp/foaf.rdf')
         repository.check_documents()
