c48770a6082601a8ce5d6cf7d9a11e6b884b78f4
carenini <carenini@1a00e267-dc06-0410-b807-9351c1609e06>
2006-05-09 16:38:23 +0000

    Conan: more work on repository

--- trunk/conan/owlrepository.py
+++ trunk/conan/owlrepository.py
@@ -14,24 +14,44 @@ class OWLRepository(Repository):
     # These two variables have to be defined in some config file!!!
     owl_classes='http://veggente.berlios.de/think/owl.n3'
     owl_rules='http://veggente.berlios.de/think/owl_rules.n3'
-    def add_document(self, uri, think=True, context=None, overwrite=True):
-        Repository.add_document(self,uri,context)
-        
-    def add_document(self, uri, think=True, context=None, overwrite=True):
-        Repository.add_document(self,uri,context)
+
+    def add_ontology(self, uri, think=True, overwrite=True):
+        """
+        @uri:string : ontology uri
+        @overwrite:boolean   : if True overwrite previously stored ontology
+        Returns:
+            0 on success
+            1 on failure
+        """
+        if (uri is None) or (uri==''):
+            return 1
+        Repository.add_document(uri)
+        import_list=self.find_imports(uri)
+        for doc in import_list:
+            Repository.add_document(doc)
         if think==True:
-            self.exec_inference(uri,'think+'+uri)
+            inferred_statements=self.exec_inference(uri,import_list,overwrite)
+            if inferred_statements is None:
+                return 1
+            for s in inferred_statements:
+                self.model.add_statement(s,RDF.Node(RDF.Uri('think+'uri)))
+        return 0
             
-    def exec_ontology_inference(self, uri_list,  results_uri):
+    def exec_ontology_inference(self, original_uri, uri_list=[], overwrite=False):
         """
-        @context_uri:char*
-        @results_uri:char* :Where to store inferred statements
+        @uri:string : ontology uri
+        @uri_list[]:string[] : list of import uri
+        @overwrite:boolean   : if True overwrite previously inferred data
+        Returns:
+            list of statements
         """
-        target=self.find_deps()
+        if (uri is None) or (uri==''):
+            return None
         cmd=[]
         # I know it this UGLY, but if it works...
         cmd.append('cwm')
         cmd.append('--rdf')
+        cwm.append(uri)
         for i in uri_list:
             cmd.append(uri_list)
         cmd.append(' --n3 ')
@@ -42,9 +62,9 @@ class OWLRepository(Repository):
         cmd.append('--purge') 
         cmd.append('--rdf')
         cwm_out=cwm.doCommand(cmd)
-        if cwm_out!=None:
-            
-        return None
+        if (cwm_out is None) or (cwm_out==[]):
+            return None
+        return self.parser.parse_string_as_stream(cwm_out,uri)
     
     def __find_location(self,uri):
         """
@@ -56,6 +76,9 @@ class OWLRepository(Repository):
                 # URI is in store
                 return self.db_uri+uri
         return uri
+    
+    def find_imports(self,uri):
+        query=SPARQLQuery('select ?obj ')
     
     def is_allowed(self, property_uri,  class_uri):
         """
